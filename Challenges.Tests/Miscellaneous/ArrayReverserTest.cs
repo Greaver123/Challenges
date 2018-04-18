ed52fd64fe363338b111910e55fe079d7161d313
p.kolodziej92@gmail.com <p.kolodziej92@gmail.com>
2018-04-18 16:48:35 +0200

    Add ArrayRevesers

---
+++ Challenges.Tests/Miscellaneous/ArrayReverserTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Challenges.Miscellaneous;
+namespace Challenges.Tests.Miscellaneous
+{
+    /// <summary>
+    /// Summary description for ArrayReverserTest
+    /// </summary>
+    [TestClass]
+    public class ArrayReverserTest
+    {
+        private ArrayReverser reverser;
+
+        public ArrayReverserTest()
+        {
+            reverser = new ArrayReverser();
+        }
+
+        [TestMethod]
+        public void Reverse_ArrayWithOddNumberOfElements_ReversedListOfNumbers()
+        {
+
+            var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            var expectedResult = new int[] { 7, 6, 5, 4, 3, 2, 1 };
+
+            var actualResult = reverser.Reverse(numbers);
+
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+
+        }
+        [TestMethod]
+        public void Reverse_ArrayWithEvenNumberOfElements_ReversedListOfNumbers()
+        {
+
+            var numbers = new int[] { 1, 2, 3, 4, 5, 6, };
+
+            var expectedResult = new int[] { 6, 5, 4, 3, 2, 1 };
+
+            var actualResult = reverser.Reverse(numbers);
+
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Reverse_EmptyArray_ThrowsArgumentExpection()
+        {
+            var numbers = new int[] { };
+            var actualResult = reverser.Reverse(numbers);
+        }
+
+        [TestMethod]
+        public void Reverse_ArrayWithOneNumber_ArrayWithOneNumber()
+        {
+            var numbers = new int[] { 1 };
+
+            var expectedResult = new int[] { 1 };
+
+            var actualResult = reverser.Reverse(numbers);
+
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Reverse_ArrayWithTwoNumbers_ArrayWithTwoNumbers()
+        {
+            var numbers = new int[] { 1, 2 };
+
+            var expectedResult = new int[] { 2, 1 };
+
+            var actualResult = reverser.Reverse(numbers);
+
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
+
---
+++ Challenges/Miscellaneous/ArrayReverser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges.Miscellaneous
+{
+    public class ArrayReverser
+    {
+
+        public int[] Reverse(int[] array)
+        {
+            if (array.Count()==0)
+            {
+                throw new ArgumentException();
+            }
+            for (int i = 0, j = array.Count()-1; i != j; i++)
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                j--;
+                if (i==j)
+                {
+                    return array;
+                }
+
+            }
+            return array;
+
+        }
+    }
+}
