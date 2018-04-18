fbc6e2ac2bee7696526967410b24671f8c5f62e3
p.kolodziej92@gmail.com <p.kolodziej92@gmail.com>
2018-04-18 18:42:55 +0200

    Add FormatNumber method

---
+++ Challenges.Tests/Miscellaneous/NumberToStringFormatterTests.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Challenges.Miscellaneous;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges.Miscellaneous.Tests
+{
+    [TestClass()]
+    public class NumberToStringFormatterTests
+    {
+        private NumberToStringFormatter formatter;
+
+        public NumberToStringFormatterTests()
+        {
+            formatter = new NumberToStringFormatter();
+        }
+        [TestMethod()]
+        public void FormatNumber_IntegerOne_StringOne()
+        {
+            int number = 1;
+            var expected = "one";
+            var actual = formatter.FormatNumber(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void FormatNumber_IntegerNinteen_StringNinteen()
+        {
+            int number = 19;
+            var expected = "ninteen";
+            var actual = formatter.FormatNumber(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FormatNumber_IntegerTwenty_StringTwenty()
+        {
+            int number = 20;
+            var expected = "twenty";
+            var actual = formatter.FormatNumber(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FormatNumber_IntegerNinty_StringNinty()
+        {
+            int number = 90;
+            var expected = "ninty";
+            var actual = formatter.FormatNumber(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FormatNumber_TwentyOne_StringTwnetyOne()
+        {
+            int number = 21;
+            var expected = "twenty one";
+            var actual = formatter.FormatNumber(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FormatNumber_NintyNine_StringNintyNine()
+        {
+            int number = 99;
+            var expected = "ninty nine";
+            var actual = formatter.FormatNumber(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FormatNumber_FourtyFive_StringFourtyFive()
+        {
+            int number = 45;
+            var expected = "fourty five";
+            var actual = formatter.FormatNumber(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FormatNumber_OneHundred_ThrowsArgumentException()
+        {
+            int number = 100;
+            var actual = formatter.FormatNumber(number);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FormatNumber_Zero_ThrowsArgumentException()
+        {
+            int number = 0;
+            var actual = formatter.FormatNumber(number);
+        }
+
+
+    }
+}
---
+++ Challenges/Miscellaneous/NumberToStringFormatter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges.Miscellaneous
+{
+
+    public class NumberToStringFormatter
+    {
+        private string[] ones = { "one", "two", "three", "for", "five", "six", " seven", "eight", "nine", "ten",
+            "eleven", "twelve", "thirteen", "forteen", "fifteen", "sixteen", "seventeen", "eighteen", "ninteen" };
+
+        private string[] tens = { "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty" };
+
+        /// <summary>
+        /// Takes input as a number and converts it into string format
+        /// </summary>
+        /// <param name="number"> number to convert. Must be between 1 and 99</param>
+        /// <returns>converted number (example: 54 converts to fifty four </returns>
+        public string FormatNumber(int number)
+        {
+            if (number < 20 && number > 0)
+            {
+                return ones[number - 1];
+            }
+            else if (number >= 20 && number <= 99)
+            {
+                if (number % 10 == 0)
+                {
+                    int index = (number / 10) - 2;
+                    return tens[index];
+                }
+                else
+                {
+                    string onesText = ones[(number % 10) - 1];
+                    string tensText = tens[(int)(number / 10) - 2];
+                    return tensText + " " + onesText;
+                }
+
+            }
+            else
+            {
+                throw new ArgumentException("Number must be between 1 and 99");
+            }
+        }
+
+
+
+    }
+}
