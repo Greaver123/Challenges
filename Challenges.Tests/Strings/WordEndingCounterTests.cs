8eca3443fd927ac6fd492f87161a2e8d88cce17a
p.kolodziej92@gmail.com <p.kolodziej92@gmail.com>
2018-04-19 19:05:56 +0200

    Add WordEndingCounter

---
+++ Challenges.Tests/Strings/WordEndingCounterTests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Challenges.Strings;
+namespace Challenges.Tests.Strings
+{
+    [TestClass]
+    public class WordEndingCounterTests
+    {
+        private WordEndingCounter counter;
+        public WordEndingCounterTests()
+        {
+            counter = new WordEndingCounter();
+        }
+
+        [TestMethod]
+        public void CounterCountWordsWithEnding_EndingY_One()
+        {
+
+            string text = "Ala ma koty";
+            string endingLetter = "y";
+
+            int expectedCount = 1;
+            int actualResult = counter.CountWordsWithEnding(text, endingLetter);
+
+            Assert.AreEqual(expectedCount, actualResult);
+
+        }
+
+        [TestMethod]
+        public void CounterCountWordsWithEnding_TwoEndingY_Two()
+        {
+
+            string text = "Alay ma koty";
+            string endingLetter = "y";
+
+            int expectedCount = 2;
+            int actualResult = counter.CountWordsWithEnding(text, endingLetter);
+
+            Assert.AreEqual(expectedCount, actualResult);
+
+        }
+
+        [TestMethod]
+        public void CounterCountWordsWithEnding_ThreeEndingY_Three()
+        {
+
+            string text = "Alay may koty";
+            string endingLetter = "y";
+
+            int expectedCount = 3;
+            int actualResult = counter.CountWordsWithEnding(text, endingLetter);
+
+            Assert.AreEqual(expectedCount, actualResult);
+
+        }
+
+        [TestMethod]
+        public void CounterCountWordsWithEnding_EmptyText_Zero()
+        {
+
+            string text = "";
+            string endingLetters = "y";
+            int expectedCount = 0;
+            int actualResult = counter.CountWordsWithEnding(text, endingLetters);
+
+            Assert.AreEqual(expectedCount, actualResult);
+
+        }
+
+        [TestMethod]
+        public void CounterCountWordsWithEnding_EmptyTextAndEndings_Zero()
+        {
+            string text = "";
+            string endingLetters = "";
+            int expectedCount = 0;
+            int actualResult = counter.CountWordsWithEnding(text, endingLetters);
+
+            Assert.AreEqual(expectedCount, actualResult);
+        }
+
+        [TestMethod]
+        public void CounterCountWordsWithEnding_TextwithYZEndings_Two()
+        {
+            string text = "Ala ma koty i psyz";
+            string endingLetters = "yz";
+            int expectedCount = 2;
+            int actualResult = counter.CountWordsWithEnding(text, endingLetters);
+
+            Assert.AreEqual(expectedCount, actualResult);
+        }
+
+        [TestMethod]
+        public void CounterCountWordsWithEnding_OnlyWhitspacetextAndTwoEndings_Zero()
+        {
+            string text = "               ";
+            string endingLetters = "YZ";
+            int expectedCount = 0;
+            int actualResult = counter.CountWordsWithEnding(text, endingLetters);
+
+            Assert.AreEqual(expectedCount, actualResult);
+        }
+
+        [TestMethod]
+        public void CounterCountWordsWithEnding_OnlyWhitspacesWithOneWordWithEndingY_One()
+        {
+            string text = "  kotyz    ";
+            string endingLetters = "YZ";
+            int expectedCount = 1;
+            int actualResult = counter.CountWordsWithEnding(text, endingLetters);
+
+            Assert.AreEqual(expectedCount, actualResult);
+        }
+
+
+    }
+}
---
+++ Challenges/Strings/WordEndingCounter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges.Strings
+{
+    /// <summary>
+    /// program that receives a string, the program then counts the number of words ending in 'y' or 'z'.
+    /// </summary>
+    public class WordEndingCounter
+    {
+        public int CountWordsWithEnding(string text, string endingLetters)
+        {
+            string[] words = text.Split(',', '.', ' ');
+            int count = 0;
+
+            if (text.Count() != 0)
+            {
+
+                foreach (var word in words)
+                {
+                    if (word == string.Empty)
+                    {
+                        continue;
+                    }
+                    else if (endingLetters.Contains(word.Last()))
+                    {
+                        count++;
+                    }
+                }
+
+            }
+            return count;
+
+        }
+    }
+}
